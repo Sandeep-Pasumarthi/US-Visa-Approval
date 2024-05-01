0c03f15bda07289cc91360a91458ec43962fefa5
Sandeep-Pasumarthi <sivasandeeppasumarthi@gmail.com>
2024-05-01 16:15:37 +0530

    data ingestion constants added

---
+++ src/constants/__init__.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+DATABASE="US_VISA"
+RAW_DATA_COLLECTION="raw"
+MONGODB_URL_KEY="MONGO_URL"
+PIPELINE="usvisa"
+ARTIFACT_DIR="artifact"
+MODEL_FILE="model.pkl"
+TARGET_COLUMN="case_status"
+CURRENT_YEAR=date.today().year
+PREPROCESSING_FILE="preprocessing.pkl"
+RAW_DATA_FILE="raw.csv"
+TRAIN_DATA_FILE="train.csv"
+TEST_DATA_FILE="test.csv"
+
+DATA_INGESTION_DIR="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR="feature_store"
+DATA_INGESTION_INGESTED_DIR="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO=0.2
