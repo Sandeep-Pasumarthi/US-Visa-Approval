87a4296ab3e364dab8ead5a86ef3d3fd736438c1
Sandeep-Pasumarthi <sivasandeeppasumarthi@gmail.com>
2024-05-02 20:08:02 +0530

    data validation config added

--- src/entity/config.py
+++ src/entity/config.py
@@ -19,9 +19,15 @@ training_pipeline_config_obj = TrainingPipelineConfig()
 
 @dataclass
 class DataIngestionConfig:
-    data_ingestion_dir=os.path.join(training_pipeline_config_obj.artifact_dir, DATA_INGESTION_DIR)
+    data_ingestion_dir = os.path.join(training_pipeline_config_obj.artifact_dir, DATA_INGESTION_DIR)
     feature_store_file_path = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, RAW_DATA_FILE)
     train_file_path = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_DATA_FILE)
     test_file_path = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_DATA_FILE)
     train_test_split_ratio = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
     raw_data_collection = RAW_DATA_COLLECTION
+
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir = os.path.join(training_pipeline_config_obj.artifact_dir, DATA_VALIDATION_DIR)
+    drift_report_file_path = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR, DATA_VALIDATION_DRIFT_REPORT_FILE)
