c3670df05e7c9fc5fb19ed778fd407bbc49d94c7
Sandeep-Pasumarthi <sivasandeeppasumarthi@gmail.com>
2024-05-02 20:07:10 +0530

    data validation constants added

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+import os
+
 DATABASE="US_VISA"
 RAW_DATA_COLLECTION="raw"
 MONGODB_URL_KEY="MONGO_URL"
@@ -12,8 +14,13 @@ PREPROCESSING_FILE="preprocessing.pkl"
 RAW_DATA_FILE="raw.csv"
 TRAIN_DATA_FILE="train.csv"
 TEST_DATA_FILE="test.csv"
+SCHEMA_FILE=os.path.join("config", "schema.yaml")
 
 DATA_INGESTION_DIR="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR="feature_store"
 DATA_INGESTION_INGESTED_DIR="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO=0.2
+
+DATA_VALIDATION_DIR="data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE="report.yaml"
