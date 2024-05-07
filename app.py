e056ae6ac1ca33c74f9372ea7599afda918934f9
Sandeep-Pasumarthi <sivasandeeppasumarthi@gmail.com>
2024-05-07 06:46:38 +0530

    app modified

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from src.pipeline.prediction import Data, Classifier
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from uvicorn import run
